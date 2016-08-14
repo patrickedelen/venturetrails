982b0610fbc9c1abd346a5487f895de49d3f0a93
Patrick Edelen <patrickedelen@gmail.com>
2016-08-13 23:43:17 -0500

    Started dynamic route handling

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,11 @@ module.exports = function(app) {
 
 	});
 }
+app.get('/*', function (req, res) {
+   db.findPage({ slug: req.url}, function (err, pageData) {
+       res.render('page-template', {
+           pageContent: pageData.content,
+           pageTitle: pageData.title
+       });
+   });
+});
